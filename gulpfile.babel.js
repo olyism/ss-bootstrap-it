74257bb6e13d40b8fbe47aeeadd6d58b06402fb0
Oly Su <oly@silverstripe.com>
2016-12-02 11:53:18 +1300

    Use requireDir to require gulp tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,19 @@
-import gulp from 'gulp';
+/**
+ * gulpfile.babel.js
+ *
+ * Rather than manage one giant configuration file responsible
+ * for creating multiple tasks, each task has been broken out into
+ * its own file in gulp/tasks. Any files in that directory get
+ * automatically required below.
+ *
+ * To add a new task, simply add a new task file that directory.
+ * gulp/tasks/default.js specifies the default set of tasks to run
+ * when you run `gulp`.
+ *
+ * You can also use ES2015.
+ */
 
-gulp.task('default', ['build', 'watch']);
-gulp.task('build', ['css:build', 'js:build']);
-require('./gulp/tasks/css')(gulp);
-require('./gulp/tasks/js')(gulp);
-require('./gulp/tasks/watch')(gulp);
+import requireDir from 'require-dir';
+
+// Require all tasks in gulp/tasks, including subfolders
+requireDir('./gulp/tasks', { recurse: true });
